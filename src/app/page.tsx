ff5f5c14850d41cd42a02cb229a8e44df76b554f
Brant Hardy <brant@branthardy.com>
2025-04-29 17:10:56 -0700

    fix build issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import { Minesweeper } from "@/components/Minesweeper";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen font-[family-name:var(--font-geist-sans)] bg-gray-900">
-      <Minesweeper />
+      <Suspense>
+        <Minesweeper />
+      </Suspense>
     </main>
   );
 }
