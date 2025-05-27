f2a0784c00fbd21171578929dba905cb8c8706ac
Brant Hardy <brant@branthardy.com>
2025-05-27 13:05:08 -0700

    turns off turbopack build for prod

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,32 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        {
+          loader: "@svgr/webpack",
+          options: {
+            svgoConfig: {
+              plugins: [
+                {
+                  name: "preset-default",
+                  params: {
+                    overrides: {
+                      removeViewBox: false,
+                    },
+                  },
+                },
+                "removeDimensions",
+              ],
+            },
+          },
+        },
+      ],
+    });
+    return config;
+  },
   turbopack: {
     rules: {
       "*.svg": {
